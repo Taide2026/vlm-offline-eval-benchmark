d8371e3f6f696a2d299775b04c22031b10ddf2bd
THChou1220 <thchou1220@gmail.com>
2026-05-01 11:29:06 +0800

    Modefy

--- script.py
+++ script.py
@@ -7,10 +7,15 @@ video_dirs = [
     "falling_off_chair"
 ]
 
-model_id = "bear7011/gemma3-4b-kinetic3K_FT"
+model_id = [
+	"google/gemma-3-4b-it",
+	"google/gemma-4-E4B-it",
+	"bear7011/gemma3-4b-kinetic3K_FT"
+]
 
 for v in video_dirs:
     video_path = f"./dataset/{v}"
     
-    os.system(f"python3 main.py --video_dir={video_path} --model_id={model_id}")
-    os.system(f"python3 llm_judge.py --video_dir={video_path} --model_id={model_id}")
+	for model in model_id:
+    	os.system(f"python3 main.py --video_dir={video_path} --model_id={model}")
+    	os.system(f"python3 llm_judge.py --video_dir={video_path} --model_id={model}")
